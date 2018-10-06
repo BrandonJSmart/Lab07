00e2dfebd5fb6e5bc6c720512443f588aa3eab45
BrandonJSmart <43220344+BrandonJSmart@users.noreply.github.com>
2018-10-06 11:04:50 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php include "inc/html-top.inc"; ?>
+
+<body>
+  <div class="container">
+
+<header>
+    <div>
+      <h1> Three students at UR</h1>
+    </div>
+</header>
+
+<article>
+	<h2>Odd Student</h2>
+	<img src="images/victoria.jpg" alt="j">
+	<h3>About Me</h3>
+	<p>asdfasdfasdfasdf.</p>
+
+	<h2>Rando 2</h2>
+	<img src="images/aliye.jpg" alt="j">
+	<h3>About me</h3>
+	<p>Ifdfdfdfdf </p>
+     
+    <h2>Rando 3</h2>
+    <img src="images/lihui.jpg" alt="j">
+    <h3>About me</h3>
+	<p>klj;lkjkljlkj </p>
+
+</article>
+<footer>
+	<p> Team Alicante - CSC174 </p>
+
+	 <ul> 
+        <li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
+        <li> <a href="http://csc174.org/assignment02/">'s page</a> </li> 
+        <li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
+    </ul>
+</footer>
+</div>
+</body>
+</html>
---
+++ subpage.php
@@ -0,0 +1,41 @@
+<?php include "inc/html-top.inc"; ?>
+
+<body>
+  <div class="container">
+
+<header>
+    <div>
+      <h1> Title about odd student </h1>
+    </div>
+</header>
+
+	<article>
+
+	<h2>Odd student</h2>
+	<h3>Employment</h3>
+	<p>ddfdfdfdfdf</p>
+	
+	<h2>Rando</h2>    
+     <h3>School</h3>
+     <p>adsfasdf</p>
+
+     <h3>Experience</h3>
+     <p>asdfasdfasdf</p>
+
+    <h2>RAndo 2</h2>
+	<h3>Experience</h3>
+	<p>asdfasdf </p>
+
+</article>
+<footer>
+	<p> Team Alicante - CSC174 </p>
+
+	<ul> 
+        <li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
+        <li> <a href="http://csc174.org/assignment02/">'s page</a> </li> 
+        <li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
+    </ul>
+</footer>
+</div>
+</body>
+</html>
