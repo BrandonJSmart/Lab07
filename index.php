cf37dfe6dc8de86cef63694c7932993b2efe2fa8
juliamyers <43220436+juliamyers@users.noreply.github.com>
2018-10-08 22:21:20 -0400

    Update index.php
    added content

--- index.php
+++ index.php
@@ -10,30 +10,30 @@
 </header>
 
 <article>
-	<h2>Odd Student</h2>
-	<img src="images/victoria.jpg" alt="j">
-	<h3>About Me</h3>
-	<p>asdfasdfasdfasdf.</p>
+	<h2>About Xinyi:</h2>
+	<img src="images/tianjin.png" alt="tianjin">
+	<p>Xinyi is a digital media study undergraduate student of University of Rochester. She is also trying to major or minor in Spanish. Her computer science skill includes basic HTML, CSS, python. Her other skills include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.</p>
 
-	<h2>Rando 2</h2>
-	<img src="images/aliye.jpg" alt="j">
-	<h3>About me</h3>
-	<p>Ifdfdfdfdf </p>
+	<h2>About Jonathan:</h2>
+	<img src="images/jroman.jpg" alt="jonathan">
+	<p>Jonathan is from Tenafly, NJ and was born September 6th, 1997. He currently attends the University of Rochester with a major in Computer Science. His hobbies include playing video games and basketball, both of which he participates in groups around campus.</p>
      
-    <h2>Rando 3</h2>
-    <img src="images/lihui.jpg" alt="j">
-    <h3>About me</h3>
-	<p>klj;lkjkljlkj </p>
+    	<h2>About Renjie:</h2>
+   	<img src="images/jerry.png" alt="renjie">
+	<p>Renjie is an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that he completed his high school at Walsingham Academy in a little town named Williamsburg. He has lived and studied in Shanghai, China for 16 years. </p>
 
 </article>
+	  
+<aside>
+	<ul> 
+        	<li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
+        	<li> <a href="http://csc174.org/assignment02/">'s page</a> </li> 
+        	<li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
+   	</ul> 
+</aside>
+	  
 <footer>
-	<p> Team Alicante - CSC174 </p>
-
-	 <ul> 
-        <li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
-        <li> <a href="http://csc174.org/assignment02/">'s page</a> </li> 
-        <li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
-    </ul>
+	Team Alicante - CSC174 
 </footer>
 </div>
 </body>
