f0ca42c8dcfcd36a2ed7586a04929a1ad4c431fc
juliamyers <43220436+juliamyers@users.noreply.github.com>
2018-10-10 00:25:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,34 @@
-<?php include "inc/html-top.inc"; ?>
+<?php include "includes/html.inc"; ?>
 
-<body>
+<body class="index">
   <div class="container">
 
 <header>
-    <div>
-      <h1> Odd student out</h1>
-    </div>
+      <h1> Alicante: Odd student out</h1>
 </header>
 
+<?php include "includes/nav.inc"; ?>
+
 <article>
-	<h2>About Xinyi:</h2>
-	<img src="images/tianjin.png" alt="tianjin">
-	<p>Xinyi is a digital media study undergraduate student of University of Rochester. She is also trying to major or minor in Spanish. Her computer science skill includes basic HTML, CSS, python. Her other skills include cooking weird food, drawing grotesque, speaking a little chin-span-glish, and chin-span-urk.</p>
-
-	<h2>About Jonathan:</h2>
-	<img src="images/jroman.jpg" alt="jonathan">
-	<p>Jonathan is from Tenafly, NJ and was born September 6th, 1997. He currently attends the University of Rochester with a major in Computer Science. His hobbies include playing video games and basketball, both of which he participates in groups around campus.</p>
-     
-    	<h2>About Renjie:</h2>
-   	<img src="images/jerry.png" alt="renjie">
-	<p>Renjie is an international student currently major in Business Analytics and minor in Computer Science at the University of Rochester. Before that he completed his high school at Walsingham Academy in a little town named Williamsburg. He has lived and studied in Shanghai, China for 16 years. </p>
-
+		<img src="images/tianjin.png" alt="tianjin">
+		<img src="images/jroman.jpg" alt="jonathan">
+		<img src="images/jerry.jpeg" alt="renjie"> 
 </article>
 	  
 <aside>
 	<ul> 
-        	<li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
-        	<li> <a href="http://csc174.org/assignment02/">'s page</a> </li> 
-        	<li> <a href="http://csc174.org/assignment02/">'s page</a></li> 
+        <li> <a href="http://csc174.org/assignment02/xchen/">Xinyi</a></li> 
+        <li> <a href="http://csc174.org/assignment02/jroman8/Assignment02/">Jonathan</a> </li> 
+        <li> <a href="http://csc174.org/assignment02/rxiao2/">Renjie</a></li> 
    	</ul> 
 </aside>
 	  
 <footer>
 	Team Alicante - CSC174 
 </footer>
-<?php include "inc/scripts.inc"; ?>
+
+<?php include "includes/scripts.inc"; ?>
+
 </div>
 </body>
 </html>
