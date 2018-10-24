e903f8b68faaeb76eb8956cfcb4c51fa4b06911f
juliamyers <43220436+juliamyers@users.noreply.github.com>
2018-10-24 14:55:12 -0400

    Resubmission files

---
+++ alicante/index.php
@@ -0,0 +1,28 @@
+<?php include "includes/html.inc"; ?>
+
+<body>
+<div class="container">
+
+<header>
+	<div class="index">
+      <h1> Team Alicante </h1>
+      <h2> Odd Student Out </h2>
+    </div>
+</header>
+
+<main>
+		<img src="images/index_image.png" alt="odd_out">
+</main>
+	  
+<footer>
+	<div class="terminal">
+		CSC 174 
+		<button onclick="window.location.href='subpage.php'">Click to Read More!</button>
+	</div>
+</footer>
+
+<?php include "includes/scripts.inc"; ?>
+
+</div>
+</body>
+</html>
